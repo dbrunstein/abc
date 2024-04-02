4af0da8bd1b97521ccbfb63f8e17fbd66dabae87
Damien <damien.brunstein@etu.unistra.fr>
2024-04-02 11:01:31 +0200

    homeview model de la semaine dernière

--- app/src/main/java/com/example/bookapp/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/bookapp/ui/home/HomeViewModel.java
@@ -1,9 +1,17 @@
 package com.example.bookapp.ui.home;
 
+import android.app.Application;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.bookapp.APIRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -15,15 +23,14 @@ public class HomeViewModel extends ViewModel {
 
     private final MutableLiveData<List<Book>> books; // faire une class book ou est convertit les
                                                     // livre en objet java
-
-    public HomeViewModel() throws JSONException {
+    private APIRequest apiRequest = new APIRequest();
+    public HomeViewModel(@NonNull Application application) throws JSONException {
         books = new MutableLiveData<>();
         //mText.setValue("WELCOME TO THE BOOK ZONE!\n ONLY BOOKS IN ANIME MOLOCH");
         //books.setValue(new JSONArray("[{\"moloch\":666,}]"));
-        ArrayList<Book> testList = new ArrayList<>();
-        testList.add(new Book(0,"H vs Zedator"));
-        Log.d("test","test");
-        books.setValue(testList);
+        RequestQueue queue = Volley.newRequestQueue(application.getApplicationContext());
+        JsonArrayRequest getRequest = apiRequest.getBooks(books);
+        queue.add(getRequest);
     }
 
     public LiveData<List<Book>> getBooks() {
